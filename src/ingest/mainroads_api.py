e888e092ec08ddf433a12a86f76358e98915ed07
NekoTRSH <konei2004@gmail.com>
2026-03-18 00:56:32 +0800

    Sets imports config and session in mainroads.py. Added gitignore and made a venv locally (not saved)

--- src/ingest/mainroads_api.py
+++ src/ingest/mainroads_api.py
@@ -10,3 +10,16 @@ import pandas as pd
 import requests
 from sqlalchemy import create_engine, text
 
+LAYER_URL = os.getenv(
+    "MAINROADS_ROAD_NETWORK_URL",
+    "https://gisservices.mainroads.wa.gov.au/arcgis/rest/services/OpenData/RoadAssets_DataPortal/MapServer/17"
+)
+
+RAW_DIR = Path(os.getenv("RAW_ROAD_DIR", "data/raw/road_network"))
+SILVER_DIR = Path(os.getenv("SILVER_DIR", "data/silver"))
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", "2000"))
+HTTP_TIMEOUT = int(os.getenv("HTTP_TIMEOUT", "60"))
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+session = requests.Session()
+
